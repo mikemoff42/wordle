2a129f46e1c2cffcc7e2d227b1bbc2e82043a461
mikemoff42 <120814501+mikemoff42@users.noreply.github.com>
2023-01-18 16:49:29 -0500

    up

--- keyboard.js
+++ keyboard.js
@@ -2,36 +2,27 @@ let letters='QWERTYUIOPASDFGHJKLZXCVBNM';
 let allKeys=[];
 let colorKeys=[];
 function KeyBoard(){
-  for (let i=0;i<letters.length;i++){
-    allKeys.push(letters.charAt(i));
-    colorKeys[i]=[220,100];
-  }
-}
-function showKeys(){
-  let xspa = width*0.05;
-  let yspa = xspa;
-  let xoffs = width*0.275;
-  let yoffs = 6*yspacing+yoff + yspa/2;
-  //height *0.8;
-  push();
-  textSize(width/30);
+  let rowCount=1;
   let counter =0;
-  
-  for (let i=0;i<26;i++){
+  let clr = [220,100];
+  for (let i=0;i<letters.length;i++){
     if (i==10){
-      yoffs+=xspa;
-      xoffs+=xspa/2;
-      counter=0;
-    } else if (i == 19){
-      yoffs+=xspa;
-      xoffs+=xspa;
+      rowCount++;
       counter=0;
     }
-    fill(colorKeys[i]);
-    square(xoffs + counter*xspa,yoffs,xspa);
-    fill(0);
-    text(allKeys[i],xoffs + counter*xspa,yoffs);
+    else if (i==19){
+      rowCount++;
+      counter=0;
+    }
+    allKeys[i] = new Key(counter,letters[i],rowCount,clr)
     counter++;
   }
-  pop();
+  
+}
+function showKeys(){
+  for (let i=0;i<26;i++){
+    allKeys[i].show();
+    
+  }
+  enterBackspace();
 }
